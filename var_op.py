f1d248ae0455ab2468cc733471a62de6630dabd0
Vishnu Parammal <38533162+Vishnuparammal@users.noreply.github.com>
2020-01-29 23:51:42 +0530

    Create var_op.py

---
+++ var_op.py
@@ -0,0 +1,46 @@
+# write these lines on python interpreter
+
+5+2
+5-2
+5*2
+5**2
+5/2
+5.0/2
+5.0//2
+5%2
+
+# what if i wish to share this calculator with my friends
+# we need to write these lines on a file
+# lets run this file using "python var_op.py"
+# there is no output ... what's wrong ??
+# when writing on file, we need to tell python whether we wish to see the output or not 
+
+print(5+2)
+print(5-2)
+print(5*2)
+print(5**2)
+print(5/2)
+print(5.0/2)
+print(5.0//2)
+print(5%2)
+
+# now what if i want to do these operations with 3 and 4
+# and then with 12453 and 738383......will you change the numbers on each line?
+# and that too, every time? ... tedious right?
+# then lets introduce variables to ease it out
+
+a = 5
+b = 2
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a**b)
+print(a/b)
+print(a//b)
+print(a%b)
+
+# now, just change the values at one place, it will change everywhere
+# but the way its printed doesnt seem very nice, right?
+# lets move on to our next module and find out some solution
+
