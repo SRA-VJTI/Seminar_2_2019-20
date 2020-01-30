b51427a6dd7b5976d05b88ff52d4d39dcd2e0021
saa1605 <saaket.agashe@gmail.com>
2020-01-30 16:46:02 +0530

    added numpy codes, blob detection code and convolution

---
+++ blob_detection.py
@@ -0,0 +1,72 @@
+import rospy 
+import sys
+import cv2
+from perception.msg import array
+from sensor_msgs.msg import Image, CameraInfo, PointCloud2
+from cv_bridge import CvBridge, CvBridgeError
+import numpy as np 
+import ros_numpy
+
+kernel = np.ones((3,3),np.uint8)
+
+lower = np.array([ 165.,   100.,  100.])
+upper = np.array([ 185., 220., 210.])
+
+frame = cv2.flip(frame, 1)
+bbox = cv2.selectROI(frame)
+obj = hsv[int(bbox[1]):int(bbox[1]+bbox[3]), int(bbox[0]):int(bbox[0]+bbox[2])]
+
+h, s, v = np.median(obj[:,:,0]), np.median(obj[:,:,1]), np.median(obj[:,:,2])
+lower = np.array([ h-10,   min(0,s-100),  min(0,v-100)])
+upper = np.array([ h+10,  max(s+100,255),  max(v+100,255)])
+
+hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+mask = cv2.inRange(hsv, lower, upper)
+mask = cv2.bilateralFilter(mask,9,75,75)
+mask = cv2.dilate(mask,kernel,iterations=1)
+
+# cv2.imshow("mask",mask)
+_, contours, hR = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+idx,current_max,counter = 0, 0, 0	
+for n in contours:
+	a = cv2.contourArea(n)
+	if a>current_max:
+		current_max=a
+		idx = counter
+	counter+=1
+
+
+# redIndex, redValue = idx, current_max
+
+#Visulization
+cv2.drawContours(frame, contours, idx, (0, 0, 255), 2)
+
+#For contour based centroid
+
+#Get contour array
+# cimg = np.zeros_like(frame)
+# cv2.drawContours(cimg, contoursRed, redIndex,(0,0,255),2)
+# pts = np.where(cimg==255)
+# # pts_pc2 = ros_numpy.point_cloud2.array_to_pointcloud2(pts)
+
+# pts = np.array(pts)
+# print(pts.shape)
+# c_row = np.mean(pts[:,0])
+# c_col = np.mean(pts[:,1])
+# arr = [c_row, c_col]
+
+#For bounding-box based centroid
+x,y,w,h = cv2.boundingRect(contours[idx])
+c_x = (x + w/2)
+c_y = (y + h/2)
+arr = [c_x,c_y]
+print(arr)
+centroid_pub.publish(arr)
+img = cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+cv2.imshow(node_name, frame)
+# pc2_pub.publish(pts_pc2)
+centroid_pub.publish(arr)
+if cv2.waitKey(10) == ord('x'):
+	cv2.DestroyAllWindows()
+		
---
+++ convolution.py
@@ -0,0 +1,79 @@
+import numpy as np
+import cv2
+
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return rightMin + (valueScaled * rightSpan)
+
+def convolve(image, kernel):
+	# grab the spatial dimensions of the image, along with
+	# the spatial dimensions of the kernel
+	(iH, iW) = image.shape[:2]
+	(kH, kW) = kernel.shape[:2]
+ 
+	# allocate memory for the output image, taking care to
+	# "pad" the borders of the input image so the spatial
+	# size (i.e., width and height) are not reduced
+
+	pad = (kW - 1) // 2
+	print(image.shape)
+	image = cv2.copyMakeBorder(image, pad, pad, pad, pad,
+		cv2.BORDER_REPLICATE)
+	print(image.shape)
+	output = np.zeros((iH, iW), dtype="float32")
+	
+	# loop over the input image, "sliding" the kernel across
+	# each (x, y)-coordinate from left-to-right and top to
+	# bottom
+	for y in np.arange(pad, iH + pad):									#change
+		for x in np.arange(pad, iW + pad):								
+			# extract the ROI of the image by extracting the
+			# *center* region of the current (x, y)-coordinates
+			# dimensions
+			roi = image[y - pad:y + pad + 1, x - pad:x + pad + 1]
+ 
+			# perform the actual convolution by taking the
+			# element-wise multiplicate between the ROI and
+			# the kernel, then summing the matrix
+			k = (roi * kernel).sum()
+ 
+			# store the convolved value in the output (x,y)-
+			# coordinate of the output image
+			output[y - pad, x - pad] = k
+	
+	
+	for i in range(output.shape[0]):
+		for j in range(output.shape[1]):
+			output[i][j] = translate(output[i][j],0,255,0,1)
+	output = np.clip(output, 0, 1)
+	# return the output image
+	return output
+
+# construct a sharpening filter
+sharpen = np.array((
+	[0, -1, 0],
+	[-1, 5, -1],
+	[0, -1, 0]), dtype="int")
+# construct the Sobel y-axis kernel
+sobelY = np.array((
+	[-1, -1, -1],
+	[0, 0, 0],
+	[1, 1, 1]), dtype="int")
+
+image = cv2.imread('image.jpg')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+convoleOutput = convolve(gray, sobelY)
+# opencvOutput = cv2.filter2D(gray, -1, kernel)
+cv2.imshow('normal image', gray)
+cv2.imshow("sharpened image", convoleOutput)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
