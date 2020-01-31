959100396bad19d823494b3ab680cfe030e7334f
Vishnu Parammal <38533162+Vishnuparammal@users.noreply.github.com>
2020-02-01 00:21:42 +0530

    added comments to numpy codes

--- openCV/numpy_arrays.py
+++ openCV/numpy_arrays.py
@@ -11,6 +11,9 @@ a[0] = 5                  # Change an element of the array
 print(a)                  # Prints "[5, 2, 3]"
 
 b = np.array([[1,2,3],[4,5,6]])    # Create a rank 2 array
+#       x    0 1 2     
+#     y 0   |1 2 3|
+#       1   |4 5 6|
 print(b.shape)                     # Prints "(2, 3)"
 print(b[0, 0], b[0, 1], b[1, 0])   # Prints "1 2 4"
 
--- openCV/numpy_indexing.py
+++ openCV/numpy_indexing.py
@@ -2,7 +2,18 @@ import numpy as np
 
 a = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
 
+#   |1  2  3  4|
+#   |5  6  7  8| 
+#   |9 10 11 12|
+
+#   a[ x , y ]
 b = a[:2, 1:3]
+
+#  :2 --- signifies all rows from start(0) to 1
+# 1:3 --- signifies all columns from 1 to 2
+# the resulting matrix is
+#   |2 3|
+#   |6 7|
 
 print(a[0, 1])
 
@@ -14,8 +25,8 @@ print(a[0, 1])
 
 a = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
 
-row_r1 = a[1, :]
-row_r2 = a[1:2, :]
+row_r1 = a[1, :]    # a[row , column]
+row_r2 = a[1:2, :]  # [:] signifies all values from start to end 
 print(row_r1, row_r1.shape)
 print(row_r2, row_r2.shape)
 
@@ -28,7 +39,9 @@ print(col_r2, col_r2.shape)
 
 a = np.array([[1, 2], [3, 4], [5, 6]])
 
-print(a[[0, 1, 2], [0, 1, 0]])
+print(a[[0, 1, 2], [0, 1, 0]])  # a[x_list, y_list]
+# here [0, 1, 2] --> x_list and [0, 1, 0] --> y_list
+# represents the coordinates (0,0) (1,1) (2,0)
 
 print(np.array([a[0, 0], a[1, 1], a[2, 0]]))
 
